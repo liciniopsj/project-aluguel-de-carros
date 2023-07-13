d35ce054bf676dcb78dc48a7bcdee7d0b8a02308
liciniopsj <licinio.psj@gmail.com>
2023-07-13 18:50:37 -0300

    Rent Constructor, Cancel Method and Finish Method

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -7,24 +7,43 @@ public class Rent
     public Vehicle Vehicle { get; set; }
     public Person Person { get; set; }
     public int DaysRented { get; set; }
+    public Type PersonType { get; }
     public double Price { get; set; }
     public RentStatus Status { get; set; }
 
     //10 - Crie o construtor de `Rent` seguindo as regras de negócio
     public Rent(Vehicle vehicle, Person person, int daysRented)
     {
-        throw new NotImplementedException();
+        Vehicle = vehicle;
+        Person = person;
+        DaysRented = daysRented;
+        PersonType = Person.GetType();
+
+        if (PersonType == typeof(PhysicalPerson))
+        {
+            Price = Vehicle.PricePerDay * daysRented;
+        }
+        else
+        {
+            Price = Vehicle.PricePerDay * daysRented;
+            Price *= 0.9;
+        }
+        
+
+        Status = RentStatus.Confirmed;
+        vehicle.IsRented = true;
+        person.Debit = Price;
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
     public void Cancel()
     {
-        throw new NotImplementedException();
+        Status = RentStatus.Canceled;
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
     public void Finish()
     {
-        throw new NotImplementedException();
+        Status = RentStatus.Finished;
     }
 }
